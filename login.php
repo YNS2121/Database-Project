104173599d920d7e896596664dafb65463882f29
Umut SAYDAM <umutsaydam24@gmail.com>
2022-12-19 10:49:28 +0300

    php eklendi

--- login.php
+++ login.php
@@ -25,22 +25,22 @@
 <body class="d-flex justify-content-center align-items-center">
 
     <div class=" container-sm col-3 bg-light p-md-5">
-        <form>
+        <form action="" method="POST">
             <div class="mb-3">
                 <label for="exampleInputEmail1" class="form-label">Email address</label>
-                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="emailAddress">
             </div>
             <div class="mb-3">
                 <label for="exampleInputPassword1" class="form-label">Password</label>
-                <input type="password" class="form-control" id="exampleInputPassword1">
+                <input type="password" class="form-control" id="exampleInputPassword1" name="password">
             </div>
             <div class="mb-3 form-check">
                 <input type="checkbox" class="form-check-input" id="exampleCheck1">
                 <label class="form-check-label" for="exampleCheck1">Check me out</label>
             </div>
             <div class="d-grid gap-2 mt-4">
-                <a href="index.php" class="btn btn-primary">Log in</a>
-                <a href="register.php" class="btn btn-primary"">Sign Up</a>
+                <button type="submit" class="btn btn-primary" name="submit">Log in</button>
+                <a href="register.php" class="btn btn-primary">Sign Up</a>
             </div>
         </form>
     </div>
@@ -50,3 +50,15 @@
 </body>
 
 </html>
+<?php
+include('db/conDB.php');
+if ($conn) {
+    if (isset($_POST["submit"])) {
+        $emailAddress = $_POST["emailAddress"];
+        $password = $_POST["password"];
+        
+    }
+} else {
+    echo "Veri tabani baglanti hatasi";
+}
+?>
